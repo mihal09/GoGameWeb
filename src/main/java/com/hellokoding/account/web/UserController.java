69cadbe9f57e0427d9f72809f396e718091a9b3a
mihal09 <mjanik1@op.pl>
2020-01-26 22:23:59 +0100

    naprawa

--- src/main/java/com/hellokoding/account/web/UserController.java
+++ src/main/java/com/hellokoding/account/web/UserController.java
@@ -11,8 +11,32 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
-
+@Controller
 public class UserController {
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private SecurityService securityService;
+
+    @Autowired
+    private UserValidator userValidator;
+
+    @RequestMapping(value = "/registration", method = RequestMethod.GET)
+    public String registration(Model model) {
+        model.addAttribute("userForm", new User());
+
+        return "registration";
+    }
+
+    @RequestMapping(value = "/registration", method = RequestMethod.POST)
+    public String registration(@ModelAttribute("userForm") User userForm, BindingResult bindingResult, Model model) {
+        userValidator.validate(userForm, bindingResult);
+
+        if (bindingResult.hasErrors()) {
+            return "registration";
+        }
+
         userService.save(userForm);
 
         securityService.autologin(userForm.getUsername(), userForm.getPasswordConfirm());
@@ -59,4 +83,8 @@ public class UserController {
 
         return "welcome";
     }
+
+
+
+
 }
